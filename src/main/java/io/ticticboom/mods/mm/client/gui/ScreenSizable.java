ebf4d70ac348b224488d4b2ddcfbf22fb76af645
ticticboooomer <115849106+ticticboooomer@users.noreply.github.com>
2025-09-04 21:37:13 +0100

    Setup hotswap arg in gradle Add tiling gui renderer

--- src/main/java/io/ticticboom/mods/mm/client/gui/ScreenSizable.java
+++
@@ -1,5 +0,0 @@
-package io.ticticboom.mods.mm.client.gui;
-
-public interface ScreenSizable {
-    void resize(int width, int height);
-}
--- src/main/java/io/ticticboom/mods/mm/client/gui/widgets/TilingBackgroundGui.java
+++ src/main/java/io/ticticboom/mods/mm/client/gui/widgets/TilingBackgroundGui.java
@@ -2,58 +2,40 @@ package io.ticticboom.mods.mm.client.gui.widgets;
 
 import io.ticticboom.mods.mm.Ref;
 import io.ticticboom.mods.mm.client.gui.AbstractWidget;
-import io.ticticboom.mods.mm.client.gui.ScreenSizable;
+import io.ticticboom.mods.mm.client.texture.GuiTextures;
 import net.minecraft.client.gui.GuiGraphics;
-import net.minecraft.client.gui.narration.NarrationElementOutput;
 import net.minecraft.resources.ResourceLocation;
 
-public class TilingBackgroundGui extends AbstractWidget implements ScreenSizable {
-    private final int padding;
-    private int width;
-    private int height;
+public class TilingBackgroundGui extends AbstractWidget {
     public static final ResourceLocation guiTexture = Ref.UiTextures.TILING_GUI;
+    private final int padding;
+    private final int width;
+    private final int height;
+    private final int tileSize = 4;
 
     public TilingBackgroundGui(int x, int y, int width, int height, int padding) {
         this.padding = padding;
         this.xPos = x;
         this.yPos = y;
-        this.width = width;
-        this.height = height;
+        this.width = (width - (this.padding * 2)) - (this.tileSize * 2);
+        this.height = (height - (this.padding * 2)) - (this.tileSize * 2);
     }
 
     @Override
     public void render(GuiGraphics guiGraphics, int mouseX, int mouseY, float partialTicks) {
-        guiGraphics.blit(guiTexture, this.xPos, this.yPos, width,4, 4, 0, 18, 4, 64, 32);
-        var leftX = this.xPos + this.padding;
-        var topY = this.yPos + this.padding;
+        var leftX = xPos + padding;
+        var rightX = leftX + width;
+        var topY = yPos + padding;
+        var bottomY = topY + height;
 
-        // tl corner
-        guiGraphics.blit(guiTexture, leftX, topY, 4, 4, 0, 0, 4, 4, 64, 32);
-    }
-
-    @Override
-    public void setFocused(boolean pFocused) {
-
-    }
-
-    @Override
-    public boolean isFocused() {
-        return false;
-    }
-
-    @Override
-    public NarrationPriority narrationPriority() {
-        return NarrationPriority.NONE;
-    }
-
-    @Override
-    public void updateNarration(NarrationElementOutput pNarrationElementOutput) {
-
-    }
-
-    @Override
-    public void resize(int width, int height) {
-        this.width = width - (this.padding * 2);
-        this.height = height - (this.padding * 2);
+        GuiTextures.TILE_GUI_NW.blit(guiGraphics, leftX, topY);
+        GuiTextures.TILE_GUI_N.blit(guiGraphics, leftX + tileSize, topY, width, tileSize);
+        GuiTextures.TILE_GUI_NE.blit(guiGraphics, rightX + tileSize, topY);
+        GuiTextures.TILE_GUI_E.blit(guiGraphics, rightX + tileSize, topY + tileSize, tileSize, height);
+        GuiTextures.TILE_GUI_SE.blit(guiGraphics, rightX + tileSize, bottomY + tileSize);
+        GuiTextures.TILE_GUI_S.blit(guiGraphics, leftX + tileSize, bottomY + tileSize, width, tileSize);
+        GuiTextures.TILE_GUI_SW.blit(guiGraphics, leftX, bottomY + tileSize);
+        GuiTextures.TILE_GUI_W.blit(guiGraphics, leftX, topY + tileSize, tileSize, height);
+        GuiTextures.TILE_GUI_C.blit(guiGraphics, leftX + tileSize, topY + tileSize, width, height);
     }
 }
